ac4f3e85fa9d533f8352ca24af6c3014fc754548
T-Vishnu <88847435+T-Vishnu@users.noreply.github.com>
2021-11-08 20:45:33 +0530

    Add files via upload

---
+++ app/models/AdvertiserWishList.java
@@ -0,0 +1,34 @@
+package models;
+
+import io.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class AdvertiserWishList extends Model {
+    @Id
+    private Integer wishID;
+    private String advertiserEmail;
+    private Integer eventID;
+
+    public AdvertiserWishList(String advertiserEmail, Integer eventID){
+        this.advertiserEmail = advertiserEmail;
+        this.eventID = eventID;
+    }
+
+    public String getAdvertiserEmail() {
+        return advertiserEmail;
+    }
+
+    public void setAdvertiserEmail(String advertiserEmail) {
+        this.advertiserEmail = advertiserEmail;
+    }
+
+    public Integer getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(Integer eventID) {
+        this.eventID = eventID;
+    }
+}
